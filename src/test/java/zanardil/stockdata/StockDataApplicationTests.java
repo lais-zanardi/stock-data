186b1152eec7557664af51d4516457c49a53420a
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-03-25 10:07:38 -0300

    first commit

---
+++ src/test/java/zanardil/stockdata/StockDataApplicationTests.java
@@ -0,0 +1,13 @@
+package zanardil.stockdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
